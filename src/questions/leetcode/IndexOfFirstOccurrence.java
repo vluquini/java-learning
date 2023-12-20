b67467524a653b57857d5f5093ec64d96025339c
Vinicius Nunes <theviniciusluquini@gmail.com>
2023-12-20 19:14:56 -0300

    Question: Index Of First Occurrence

---
+++ src/questions/leetcode/IndexOfFirstOccurrence.java
@@ -0,0 +1,34 @@
+package questions.leetcode;
+
+public class IndexOfFirstOccurrence {
+    public static void main(String[] args) {
+        String haystack = "ssdbutssdsad";
+        String needle = "sad";
+        int result = 0;
+
+//        System.out.println(result = firstOccurrence(haystack, needle));
+        System.out.println(result = firstOccurrence2(haystack, needle));
+    }
+    public static int firstOccurrence(String haystack, String needle){
+        return haystack.indexOf(needle);
+    }
+
+    public static int firstOccurrence2(String haystack, String needle){
+        int j = 0;
+        int index = 0;
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if(haystack.charAt(i) == needle.charAt(j)){
+                j++;
+            }else {
+                j = 0;
+                i = index;
+                index = i + 1;
+            }
+            if (j == needle.length()){
+                return index;
+            }
+        }
+        return -1;
+    }
+}
