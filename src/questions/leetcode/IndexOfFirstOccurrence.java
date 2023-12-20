ad3084285745c0e3c30d34b2d178f4bd3aba5779
Vinicius Nunes <theviniciusluquini@gmail.com>
2023-12-20 19:25:06 -0300

    Solucao melhorada

--- src/questions/leetcode/IndexOfFirstOccurrence.java
+++ src/questions/leetcode/IndexOfFirstOccurrence.java
@@ -7,7 +7,8 @@ public class IndexOfFirstOccurrence {
         int result = 0;
 
 //        System.out.println(result = firstOccurrence(haystack, needle));
-        System.out.println(result = firstOccurrence2(haystack, needle));
+//        System.out.println(result = firstOccurrence2(haystack, needle));
+        System.out.println(result = firstOccurrence3(haystack, needle));
     }
     public static int firstOccurrence(String haystack, String needle){
         return haystack.indexOf(needle);
@@ -27,6 +28,25 @@ public class IndexOfFirstOccurrence {
             }
             if (j == needle.length()){
                 return index;
+            }
+        }
+        return -1;
+    }
+
+    public static int firstOccurrence3(String haystack, String needle){
+        int j = 0;
+        int start = 0;
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if(haystack.charAt(i) == needle.charAt(j)){
+                j++;
+                if (j == needle.length()){
+                    return start;
+                }
+            }else {
+                i = start;
+                start++;
+                j = 0;
             }
         }
         return -1;
