6679317a23a3b8b0a2962fcd5c4a4fd69416fce5
Vinicius Nunes <theviniciusluquini@gmail.com>
2024-01-12 18:00:45 -0300

    att: question remove duplicates from sortred array

--- src/questions/leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/questions/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -5,15 +5,32 @@ import java.util.HashSet;
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        HashSet<Integer> diffs = new HashSet<>();
+
+        System.out.println(nums.length);
+        System.out.println(removeDuplicates(nums));
+        System.out.println(nums.length);
+
+//        System.out.println(removeDuplicates2(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
         int k = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            // O método 'add' retorna 'false' caso seja inserido um valor já existente no Set.
-            if (diffs.add(nums[i])) {
+            if (seen.add(nums[i])) {
                 nums[k++] = nums[i];
             }
         }
-        System.out.println(k);
+        return k;
+    }
+
+    public static int removeDuplicates2(int[] nums) {
+//        int i = 0;
+        int i = nums.length > 0 ? 1 : 0;
+        for (int n : nums)
+            if (n > nums[i-1])
+                nums[i++] = n;
+        return i;
     }
 }
