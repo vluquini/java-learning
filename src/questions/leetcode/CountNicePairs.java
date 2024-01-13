1dd7003bf7a6afa86213730b86debe14570d4649
Vinicius Nunes <theviniciusluquini@gmail.com>
2024-01-12 22:46:49 -0300

    att: question count nice pairs

--- src/questions/leetcode/CountNicePairs.java
+++ src/questions/leetcode/CountNicePairs.java
@@ -5,7 +5,13 @@ import java.util.Map;
 
 public class CountNicePairs {
     public static void main(String[] args) {
-        int[] nums = {13,10,35,24,76};
+        int[] nums = {42,11,1,97};
+
+        System.out.println(countNicePairs(nums));
+        System.out.println(countNicePairs2(nums));
+    }
+
+    public static int countNicePairs(int[] nums){
         int pairs = 0;
         int mod = 1000000007;
 
@@ -24,7 +30,19 @@ public class CountNicePairs {
             // Se houver k pares de um determinado valor diff, a contribuição para o total será k * (k - 1) / 2
             pairs = (int) ((pairs + longCount * (longCount - 1) / 2) % mod);
         }
-        System.out.println(pairs);
+        return pairs;
+    }
+
+    public static int countNicePairs2(int[] nums){
+        int pairs = 0, mod = (int)1e9 + 7;
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int a : nums) {
+            int b = reversed(a), v = count.getOrDefault(a - b, 0);
+            count.put(a - b, v + 1);
+            pairs = (pairs + v) % mod;
+        }
+        return pairs;
     }
 
     private static int reversed(int num) {
