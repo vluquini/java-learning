a40e9fa7859d8a65bf06d03a5d40b1d08a98f25d
Vinicius Nunes <theviniciusluquini@gmail.com>
2024-03-06 21:03:58 -0300

    melhorando exemplo hashset

--- src/datastructures/sets/ExemploHashSet.java
+++ src/datastructures/sets/ExemploHashSet.java
@@ -17,6 +17,10 @@ public class ExemploHashSet {
         // Exibe os elementos do HashSet
         System.out.println("Elementos do HashSet: " + numeros);
 
+        // Iterando sobre o set
+        for(Integer i : numeros)
+            System.out.println(i);
+
         // Verifica se um elemento está presente no HashSet
         boolean contemNumero20 = numeros.contains(20);
         System.out.println("O HashSet contém o número 20? " + contemNumero20);
@@ -26,6 +30,14 @@ public class ExemploHashSet {
 
         // Exibe os elementos do HashSet após a remoção
         System.out.println("Elementos do HashSet após a remoção: " + numeros);
+
+        HashSet<Integer> numeros2 = new HashSet<>(numeros);
+        numeros2.add(99);
+        numeros2.add(123);
+        // Remove os elementos de 'numeros2' que não há em 'numeros'
+        numeros2.retainAll(numeros);
+        System.out.println(numeros2);
+
     }
 }
 
